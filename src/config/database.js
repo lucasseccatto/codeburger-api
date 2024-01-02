e5de9397d66dc3327285314761e62df63efe9791
lucasseccatto <lucasseccatto1@gmail.com>
2024-01-02 10:59:47 -0300

    atualizacao da url de conexao do banco de dados postgresql

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-  url: 'postgresql://postgres:g1g2E4AA*a**aCe2aC53cbDD*aE*FgbC@monorail.proxy.rlwy.net:28809/railway',
+  url: 'postgresql://postgres:254dDed4baFfD6CE641B2FB-2BA*3e-g@viaduct.proxy.rlwy.net:25632/railway',
   define: {
     timespamps: true,
     underscored: true,
